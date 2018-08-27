49ca6cf974e96a0eea064df669f0c526465e88fe
damienbod <damien_bod@hotmail.com>
2018-08-27 13:18:50 +0200

    small fixes

--- content/StsServerIdentity/Config.cs
+++ content/StsServerIdentity/Config.cs
@@ -15,9 +15,7 @@ namespace StsServerIdentity
             {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile(),
-                new IdentityResources.Email(), 
-                new IdentityResource("dataeventrecordsscope",new []{ "role", "admin", "user", "dataEventRecords", "dataEventRecords.admin" , "dataEventRecords.user" } ),
-                new IdentityResource("securedfilesscope",new []{ "role", "admin", "user", "securedFiles", "securedFiles.admin", "securedFiles.user"} )
+                new IdentityResources.Email()
             };
         }
 
@@ -40,22 +38,6 @@ namespace StsServerIdentity
                         }
                     },
                     UserClaims = { "role", "admin", "user", "dataEventRecords", "dataEventRecords.admin", "dataEventRecords.user" }
-                },
-                new ApiResource("securedFiles")
-                {
-                    ApiSecrets =
-                    {
-                        new Secret("securedFilesSecret".Sha256())
-                    },
-                    Scopes =
-                    {
-                        new Scope
-                        {
-                            Name = "securedfiles",
-                            DisplayName = "Scope for the securedFiles ApiResource"
-                        }
-                    },
-                    UserClaims = { "role", "admin", "user", "securedFiles", "securedFiles.admin", "securedFiles.user" }
                 }
             };
         }
@@ -98,81 +80,11 @@ namespace StsServerIdentity
                     AllowedScopes = new List<string>
                     {
                         "openid",
-                        "dataEventRecords",
-                        "dataeventrecordsscope",
-                        "securedFiles",
-                        "securedfilesscope",
                         "role",
                         "profile",
                         "email"
                     }
-                },
-                new Client
-                {
-                    ClientName = "angularclientidtokenonly",
-                    ClientId = "angularclientidtokenonly",
-                    AccessTokenType = AccessTokenType.Reference,
-                    AccessTokenLifetime = 360,// 120 seconds, default 60 minutes
-                    IdentityTokenLifetime = 30,
-                    AllowedGrantTypes = GrantTypes.Implicit,
-                    AlwaysIncludeUserClaimsInIdToken = true,
-                    AllowAccessTokensViaBrowser = true,
-                    RedirectUris = new List<string>
-                    {
-                        "https://localhost:44372",
-                        "https://localhost:44372/silent-renew.html"
-
-                    },
-                    PostLogoutRedirectUris = new List<string>
-                    {
-                        "https://localhost:44372/Unauthorized"
-                    },
-                    AllowedCorsOrigins = new List<string>
-                    {
-                        "https://localhost:44372",
-                        "http://localhost:44372"
-                    },
-                    AllowedScopes = new List<string>
-                    {
-                        "openid",
-                        "dataEventRecords",
-                        "dataeventrecordsscope",
-                        "securedFiles",
-                        "securedfilesscope",
-                        "role",
-                        "profile",
-                        "email"
-                    }
-},
-                new Client
-                {
-                    ClientName = "angularjsclient",
-                    ClientId = "angularjsclient",
-                    AllowedGrantTypes = GrantTypes.Implicit,
-                    AllowAccessTokensViaBrowser = true,
-                    RedirectUris = new List<string>
-                    {
-                        "https://localhost:44376/authorized"
-                    },
-                    PostLogoutRedirectUris = new List<string>
-                    {
-                        "https://localhost:44346/unauthorized.html"
-                    },
-                    AllowedCorsOrigins = new List<string>
-                    {
-                        "https://localhost:44346"
-                    },
-                    AllowedScopes = new List<string>
-                    {
-                        "openid",
-                        "email",
-                        "profile",
-                        "dataEventRecords",
-                        "dataeventrecordsscope",
-                        "securedFiles",
-                        "securedfilesscope",
-                    }
-                },
+                }
             };
         }
     }
