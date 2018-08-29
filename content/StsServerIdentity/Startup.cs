78e69074882205beb4a0d7d1fcb768b6eedb96fa
damienbod <damien_bod@hotmail.com>
2018-08-29 11:41:59 +0200

    removing de-CH and adding gsw-CH_ZH

--- content/StsServerIdentity/Startup.cs
+++ content/StsServerIdentity/Startup.cs
@@ -106,7 +106,6 @@ namespace StsServerIdentity
                             new CultureInfo("en-US"),
                             new CultureInfo("de-DE"),
                             new CultureInfo("it-IT"),
-                            new CultureInfo("de-CH"),
                             new CultureInfo("gsw-CH")
                         };
 
