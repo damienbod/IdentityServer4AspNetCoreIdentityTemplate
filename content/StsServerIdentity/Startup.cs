1e79bb9d3ceee3ee5f434a8de11c0a943cf6db20
damienbod <damien_bod@hotmail.com>
2018-08-29 05:14:56 +0200

    renaming ch-CH to de-CH (dialekt)

--- content/StsServerIdentity/Startup.cs
+++ content/StsServerIdentity/Startup.cs
@@ -105,7 +105,8 @@ namespace StsServerIdentity
                         {
                             new CultureInfo("en-US"),
                             new CultureInfo("de-DE"),
-                            new CultureInfo("it-IT")
+                            new CultureInfo("it-IT"),
+                            new CultureInfo("de-CH")
                         };
 
                     options.DefaultRequestCulture = new RequestCulture(culture: "de-DE", uiCulture: "de-DE");
