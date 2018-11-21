b807a660919ffc6eb460386aa547d370dacfe27e
damienbod <damien_bod@hotmail.com>
2018-11-21 19:39:18 +0100

    comment out example code

--- content/StsServerIdentity/Config.cs
+++ content/StsServerIdentity/Config.cs
@@ -23,22 +23,23 @@ namespace StsServerIdentity
         {
             return new List<ApiResource>
             {
-                new ApiResource("dataEventRecords")
-                {
-                    ApiSecrets =
-                    {
-                        new Secret("dataEventRecordsSecret".Sha256())
-                    },
-                    Scopes =
-                    {
-                        new Scope
-                        {
-                            Name = "dataeventrecords",
-                            DisplayName = "Scope for the dataEventRecords ApiResource"
-                        }
-                    },
-                    UserClaims = { "role", "admin", "user", "dataEventRecords", "dataEventRecords.admin", "dataEventRecords.user" }
-                }
+                // example code
+                //new ApiResource("dataEventRecords")
+                //{
+                //    ApiSecrets =
+                //    {
+                //        new Secret("dataEventRecordsSecret".Sha256())
+                //    },
+                //    Scopes =
+                //    {
+                //        new Scope
+                //        {
+                //            Name = "dataeventrecords",
+                //            DisplayName = "Scope for the dataEventRecords ApiResource"
+                //        }
+                //    },
+                //    UserClaims = { "role", "admin", "user", "dataEventRecords", "dataEventRecords.admin", "dataEventRecords.user" }
+                //}
             };
         }
 
@@ -52,39 +53,40 @@ namespace StsServerIdentity
             // client credentials client
             return new List<Client>
             {
-                new Client
-                {
-                    ClientName = "angularclient",
-                    ClientId = "angularclient",
-                    AccessTokenType = AccessTokenType.Reference,
-                    AccessTokenLifetime = 330,// 330 seconds, default 60 minutes
-                    IdentityTokenLifetime = 30,
-                    AllowedGrantTypes = GrantTypes.Implicit,
-                    AllowAccessTokensViaBrowser = true,
-                    RedirectUris = new List<string>
-                    {
-                        "https://localhost:44311",
-                        "https://localhost:44311/silent-renew.html"
+                // example code
+                //new Client
+                //{
+                //    ClientName = "angularclient",
+                //    ClientId = "angularclient",
+                //    AccessTokenType = AccessTokenType.Reference,
+                //    AccessTokenLifetime = 330,// 330 seconds, default 60 minutes
+                //    IdentityTokenLifetime = 30,
+                //    AllowedGrantTypes = GrantTypes.Implicit,
+                //    AllowAccessTokensViaBrowser = true,
+                //    RedirectUris = new List<string>
+                //    {
+                //        "https://localhost:44311",
+                //        "https://localhost:44311/silent-renew.html"
 
-                    },
-                    PostLogoutRedirectUris = new List<string>
-                    {
-                        "https://localhost:44311/unauthorized",
-                        "https://localhost:44311"
-                    },
-                    AllowedCorsOrigins = new List<string>
-                    {
-                        "https://localhost:44311",
-                        "http://localhost:44311"
-                    },
-                    AllowedScopes = new List<string>
-                    {
-                        "openid",
-                        "role",
-                        "profile",
-                        "email"
-                    }
-                }
+                //    },
+                //    PostLogoutRedirectUris = new List<string>
+                //    {
+                //        "https://localhost:44311/unauthorized",
+                //        "https://localhost:44311"
+                //    },
+                //    AllowedCorsOrigins = new List<string>
+                //    {
+                //        "https://localhost:44311",
+                //        "http://localhost:44311"
+                //    },
+                //    AllowedScopes = new List<string>
+                //    {
+                //        "openid",
+                //        "role",
+                //        "profile",
+                //        "email"
+                //    }
+                //}
             };
         }
     }
