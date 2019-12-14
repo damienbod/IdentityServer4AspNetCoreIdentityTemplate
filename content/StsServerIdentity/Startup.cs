8658a93652f760b0e407c02c0503438ed3cc3d0c
damienbod <damien_bod@hotmail.com>
2019-12-14 10:46:06 +0100

    code clean up

--- content/StsServerIdentity/Startup.cs
+++ content/StsServerIdentity/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Localization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
