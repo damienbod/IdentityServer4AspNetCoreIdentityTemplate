a993476052c13d5cf2cb7da24c5fa04403968abc
damienbod <damien_bod@hotmail.com>
2020-06-27 10:14:29 +0200

    code clean up

--- content/StsServerIdentity/Models/ApplicationUser.cs
+++ content/StsServerIdentity/Models/ApplicationUser.cs
@@ -5,7 +5,5 @@ namespace StsServerIdentity.Models
     public class ApplicationUser : IdentityUser
     {
         public bool IsAdmin { get; set; }
-        public string DataEventRecordsRole { get; set; }
-        public string SecuredFilesRole { get; set; }
     }
 }
