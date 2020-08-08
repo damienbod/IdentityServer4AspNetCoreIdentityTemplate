21c35795ea3189b6d75b3fb45a173c6d539539f4
damienbod <damien_bod@hotmail.com>
2020-08-08 16:47:29 +0200

    Adding a Encode

--- content/StsServerIdentity/Controllers/AccountController.cs
+++ content/StsServerIdentity/Controllers/AccountController.cs
@@ -272,7 +272,7 @@ namespace StsServerIdentity.Controllers
                     // WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                     //var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
                     //await _emailSender.SendEmailAsync(model.Email, "Confirm your account",
-                    //    $"Please confirm your account by clicking this link: <a href='{callbackUrl}'>link</a>");
+                    //    $"Please confirm your account by clicking this link: <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>link</a>");
                     //await _signInManager.SignInAsync(user, isPersistent: false);
                     //_logger.LogInformation(3, "User created a new account with password.");
                     return RedirectToLocal(returnUrl);
